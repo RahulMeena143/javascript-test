152fb962d8590555d148adc545b2dbee68f6f13b
Rahul Meena <rahulmeena@gmail.com>
2026-09-11 10:40:00 +0530

    Some Oprection with data

---
+++ 01_basics/02_ConversionOprection.js
@@ -0,0 +1,17 @@
+let Score = "100abc";
+
+//console.log(typeof Score); // string
+//console.log(typeof "Score");
+
+//let valueInNumber = Number(Score);
+//console.log(typeof valueInNumber); 
+
+//console.log(valueInNumber); // NaN
+
+ 
+
+let isLoggedin = 1;
+
+let boolenisLoggedin = Boolean(isLoggedin);
+console.log(boolenisLoggedin);
+
