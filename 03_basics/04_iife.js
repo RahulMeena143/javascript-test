026c899bfa45448d378b63d54a165d6ece200c8a
Rahul Meena <rahulmeena@gmail.com>
2026-09-12 17:02:43 +0530

    iife in js

---
+++ 03_basics/04_iife.js
@@ -0,0 +1,17 @@
+//--------Immediately Invoked Function Expressions (IIFE)
+//        Normal iife 
+(function chai(){
+
+    userName = "Rahul "
+    console.log(`DB CONNECED`);
+    
+}
+)();
+// ---------------Adavance Matheds------------
+( () => {
+
+    userName = "Rahul "
+    console.log(`DB CONNECED`);
+    
+}
+)()
